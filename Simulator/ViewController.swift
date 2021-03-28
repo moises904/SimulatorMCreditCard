655c870f8f5199faa77b7b860ae16a4b56a96f9d
Moises Almeyda <moisesalmeyda@bcp.com.pe>
2021-03-28 02:19:08 -0500

    Initial Commit

---
+++ Simulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simulator
+//
+//  Created by Moises Almeyda on 28/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
