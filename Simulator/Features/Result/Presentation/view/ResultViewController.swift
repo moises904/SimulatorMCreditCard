d1cccb26056e249fe904abc95b2f112cb040b659
Moises Almeyda <moisesalmeyda@bcp.com.pe>
2021-04-14 11:56:00 -0500

    Add visualization result simulation

--- Simulator/Features/Result/Presentation/view/ResultViewController.swift
+++ Simulator/Features/Result/Presentation/view/ResultViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet weak var titleResultLabel: UILabel!
+    @IBOutlet weak var amountResultLabel: UILabel!
+    @IBOutlet weak var firstDatePaymentLabel: UILabel!
     private var resultSimulator: ResultSimulatedResponse?
+
     
     @IBAction func clickReturSimulate(_ sender: Any) {
         
@@ -17,11 +21,21 @@ class ResultViewController: UIViewController {
     }
     override func viewDidLoad() {
         self.navigationItem.title = "Simulador de Compras con Tarjeta"
-        
+        showResultSimulate()
     }
     
     func loadDataResultSimulator(resultData: ResultSimulatedResponse) {
         self.resultSimulator = resultData
-        print(resultSimulator?.response.firstQuote)
     }
+    
+    func showResultSimulate() {
+        let symbolCurrency:String? =  self.resultSimulator?.response.money
+        let amount:String? = self.resultSimulator?.response.quotes
+        
+        let amountWithCurrency = "\(symbolCurrency!) \(amount!)"
+        self.amountResultLabel.text = amountWithCurrency
+        let firstDay =   "Fecha primera cuota:" + (self.resultSimulator?.response.firstQuote)!
+        firstDatePaymentLabel.text = firstDay
+    }
+        
 }
